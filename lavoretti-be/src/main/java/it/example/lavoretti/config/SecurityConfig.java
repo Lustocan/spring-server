95f7a43e1d83fb9946799cfee467224c7944516b
wcatalansuazo <washingtoncatalan@playtomic.io>
2024-05-18 17:36:45 +0200

    feat: Disable spring filter.

---
+++ lavoretti-be/src/main/java/it/example/lavoretti/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package it.example.lavoretti.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll()
+                                   // TODO once implemented uncomment this
+                                   //  .anyRequest().authenticated()
+            );
+        return http.build();
+    }
+
+}
--- lavoretti-be/src/main/java/it/example/lavoretti/controller/LoginController.java
+++ lavoretti-be/src/main/java/it/example/lavoretti/controller/LoginController.java
@@ -1,5 +1,18 @@
 package it.example.lavoretti.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/v1/login")
 public class LoginController {
+
+    // hello
+    @GetMapping
+    public ResponseEntity<String> login() {
+        return ResponseEntity.ok("Hello, World!");
+    }
 
 }
